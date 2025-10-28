c770403d194937151ef92e0b68af75b1956df837
mailidpwd <mailidpwd@gmail.com>
2025-10-28 12:48:21 +0530

    Simplify App.js

--- App.js
+++ App.js
@@ -1,41 +1,20 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 import DashboardScreen from './src/screens/DashboardScreen';
-import QuizHubScreen from './src/screens/QuizHubScreen';
-import TransferScreen from './src/screens/TransferScreen';
-import DonateScreen from './src/screens/DonateScreen';
-import MoodAssessmentScreen from './src/screens/MoodAssessmentScreen';
-import { Provider } from 'react-redux';
-import { store } from './src/store';
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function MainTabs() {
-  return (
-    <Tab.Navigator screenOptions={{ headerShown: true }}>
-      <Tab.Screen name="Dashboard" component={DashboardScreen} />
-      <Tab.Screen name="Transfer" component={TransferScreen} />
-      <Tab.Screen name="Donate" component={DonateScreen} />
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="MainTabs" component={MainTabs} />
-            <Stack.Screen name="MoodAssessment" component={MoodAssessmentScreen} />
-            <Stack.Screen name="QuizHub" component={QuizHubScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </Provider>
+    <View style={styles.container}>
+      <DashboardScreen />
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
